78ee49f24b9597050c307f63e50bf8f6e3bc2ac5
Liler Jee <lilerjee@protonmail.com>
2020-09-16 13:22:39 +0800

    get IP from header X-Forwarded-For to fit to reverse proxy server

--- django_comments/views/comments.py
+++ django_comments/views/comments.py
@@ -100,7 +100,10 @@ def post_comment(request, next=None, using=None):
 
     # Otherwise create the comment
     comment = form.get_comment_object(site_id=get_current_site(request).id)
-    comment.ip_address = request.META.get("REMOTE_ADDR", None) or None
+    if 'HTTP_X_FORWARDED_FOR' in request.META:
+        comment.ip_address = request.META['HTTP_X_FORWARDED_FOR']
+    else:
+        comment.ip_address = request.META.get("REMOTE_ADDR", None) or None
     if request.user.is_authenticated:
         comment.user = request.user
 
